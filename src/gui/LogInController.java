db9fc8d12f8768f0a3c2686a0a5f019c7de65706
rheijuan <rhei_juan@dlsu.edu.ph>
2018-04-09 10:39:56 +0800

    updated doctor controller

--- src/gui/LogInController.java
+++ src/gui/LogInController.java
@@ -2,27 +2,40 @@ package gui;
 
 import database.DBController;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 
 import database.User;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
-import java.awt.*;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class LogInController {
+public class LogInController implements Initializable {
 
     @FXML private TextField userText;
 
     @FXML private TextField passwordText;
 
+    @FXML private Label invalidLabel;
+
     @FXML
-    private void enterClinic() {
-        ObservableList<User> users = DBController.getUsers();
+    private void enterClinic(ActionEvent event) {
 
         for (User user : users) {
             if (user.getName().equals(userText.getText()) &&
                     user.getPassword().equals(passwordText.getText())) {
                 switch (user.getRole()) {
                     case "Doctor":
+                        changeScene(event, "doctorscreen.fxml");
                         // TODO load the screen of the Doctor
                         System.out.println("The doctor is in");
                         break;
@@ -35,11 +48,38 @@ public class LogInController {
                         System.out.println("The patient is in");
                         break;
                     default:
-                        // TODO load the invalid screen
+                        invalidLabel.setVisible(true);
                         break;
                 }
             }
         }
     }
 
+    private void changeScene(ActionEvent event, String url) {
+        Parent viewParent;
+        try {
+            viewParent = FXMLLoader.load(getClass().getResource(url));
+            Scene viewScene = new Scene(viewParent);
+            Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+
+            window.setScene(viewScene);
+            window.show();
+            window.centerOnScreen();
+        }catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        System.out.println("Hello");
+        users = DBController.getUsers();
+
+        for (User user : users)
+            System.out.println(user.toString());
+
+        invalidLabel.setVisible(false);
+    }
+
+    private ObservableList<User> users;
 }
