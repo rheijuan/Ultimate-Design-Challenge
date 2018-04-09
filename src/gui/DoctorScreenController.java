6b222003b6cec8c8cfda6acbddff4637894f80b1
sairakaye <30943505+sairakaye@users.noreply.github.com>
2018-04-09 16:30:09 +0800

    Can detect day selected and year and month.

--- src/gui/DoctorScreenController.java
+++ src/gui/DoctorScreenController.java
@@ -4,7 +4,6 @@ import database.Appointment;
 import database.DBController;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
@@ -76,30 +75,6 @@ public class DoctorScreenController implements Initializable {
     }
 
     @FXML
-    private void createNext() {
-        if (monthToday == 11) {
-            monthToday = 0;
-            yearToday += 1;
-        } else
-            monthToday += 1;
-
-        dateLabel.setText(convert(monthToday) + " " + dayToday + ", " + yearToday);
-        refreshCalendar(monthToday, yearToday, dayToday, "");
-    }
-
-    @FXML
-    private void createPrev() {
-        if (monthToday == 0) {
-            monthToday = 11;
-            yearToday -= 1;
-        } else
-            monthToday -= 1;
-
-        dateLabel.setText(convert(monthToday) + " " + dayToday + ", " + yearToday);
-        refreshCalendar(monthToday, yearToday, dayToday, "");
-    }
-
-    @FXML
     private void displayDateView() {
 
 
@@ -110,7 +85,6 @@ public class DoctorScreenController implements Initializable {
     private void createAppointment() {
         profilePane.setVisible(false);
         createPane.setVisible(true);
-        miniDateCalendar.setText(convert(monthToday) + " " + dayToday + ", " + yearToday);
 
         datePicker.getChildren().clear();
 
@@ -132,8 +106,8 @@ public class DoctorScreenController implements Initializable {
     private void cancelAdding() {
         createPane.setVisible(false);
         profilePane.setVisible(true);
-        refreshCalendar(monthToday, yearToday, daySelected, "mini");
     }
+
 
     @FXML
     private ObservableList<DayTableItem> getDayAppointments() {
@@ -289,13 +263,9 @@ public class DoctorScreenController implements Initializable {
             }
 
             GridPane finalTemp = temp;
-<<<<<<< HEAD
             int finalI = i;
             button.setOnAction(event -> {
                 boolean t = false;
-=======
-            button.setOnAction((ActionEvent event) -> {
->>>>>>> 4e85d02df335e67ee56b88b0b0710629a55596da
                 daySelected = Integer.parseInt(((Button) event.getSource()).getText());
                 button.setStyle("-fx-font-family: 'Avenir 85 Heavy'; -fx-font-size: 10px; -fx-background-color:  #dc654d; -fx-text-fill: #FFFFFF");
 
@@ -306,17 +276,7 @@ public class DoctorScreenController implements Initializable {
 
                 for (Node node : finalTemp.getChildren()) {
                     if (node instanceof Button && Integer.parseInt(((Button) node).getText()) != daySelected) {
-<<<<<<< HEAD
 
-=======
-                        if (Calendar.equals("mini")) {
-                            // TODO implement the thingymobob
-                            node.setStyle("-fx-font-family: 'Avenir 85 Heavy'; -fx-font-size: 10px; -fx-background-color: transparent; -fx-text-fill: #FFFFFF");
-                        }
-                        else {
-                            node.setStyle("-fx-font-family: 'Avenir 85 Heavy'; -fx-font-size: 10px; -fx-background-color: transparent; -fx-text-fill: #000000");
-                        }
->>>>>>> 4e85d02df335e67ee56b88b0b0710629a55596da
                     }
 //                    if (node instanceof Button && Integer.parseInt(((Button) node).getText()) != daySelected) {
 //                        for (Appointment app : appointments)
@@ -340,11 +300,7 @@ public class DoctorScreenController implements Initializable {
 
             for (Appointment app: appointments)
                 if(eventToday(app, i)) {
-<<<<<<< HEAD
                     button.setStyle("-fx-font-family: 'Avenir 85 Heavy'; -fx-font-size: 10px; -fx-background-color: transparent; -fx-text-fill: #98ff98");
-=======
-                    button.setStyle("-fx-font-family: 'Avenir 85 Heavy'; -fx-font-size: 10px; -fx-background-color: transparent; -fx-text-fill: #00ff90");
->>>>>>> 4e85d02df335e67ee56b88b0b0710629a55596da
                     break;
                 }
 
