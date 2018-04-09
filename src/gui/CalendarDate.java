d3563531424d90f1c1f4d8340721b6fcb60d96ea
sairakaye <30943505+sairakaye@users.noreply.github.com>
2018-04-09 15:56:00 +0800

    Secretary changes.

---
+++ src/gui/CalendarDate.java
@@ -0,0 +1,67 @@
+package gui;
+
+import java.util.GregorianCalendar;
+
+public class CalendarDate {
+
+    private static int yearBound, monthBound, dayBound, yearToday, monthToday, dayToday;
+
+    public CalendarDate(){
+        GregorianCalendar cal = new GregorianCalendar();
+        dayBound = cal.get(GregorianCalendar.DAY_OF_MONTH);
+        monthBound = cal.get(GregorianCalendar.MONTH);
+        yearBound = cal.get(GregorianCalendar.YEAR);
+        dayToday = dayBound;
+        monthToday = monthBound;
+        yearToday = yearBound;
+    }
+
+    public void setDayBound(int dayBound) {
+        this.dayBound = dayBound;
+    }
+
+    public void setMonthBound(int monthBound) {
+        this.monthBound = monthBound;
+    }
+
+    public void setYearBound(int yearBound) {
+        this.yearBound = yearBound;
+    }
+
+    public void setMonthToday(int monthToday) {
+        this.monthToday = monthToday;
+    }
+
+    public void setYearToday(int yearToday) {
+        this.yearToday = yearToday;
+    }
+
+    public int getYearBound() {
+        return yearBound;
+    }
+
+    public int getMonthBound() {
+        return monthBound;
+    }
+
+    public int getDayBound() {
+        return dayBound;
+    }
+
+    public int getDayToday() {
+        return dayToday;
+    }
+
+    public int getYearToday() {
+        return yearToday;
+    }
+
+    public int getMonthToday() {
+        return monthToday;
+    }
+
+    public int getMaxDay() {
+        GregorianCalendar cal = new GregorianCalendar(yearBound, monthBound, 1);
+        return cal.getActualMaximum(GregorianCalendar.DAY_OF_MONTH);
+    }
+}
--- src/gui/SecretaryScreenController.java
+++ src/gui/SecretaryScreenController.java
@@ -23,65 +23,55 @@ public class SecretaryScreenController {
 	public Button btnPrev;
 	@FXML
 	public Label dateLabel;
-	
+
 	private int yearBound;
 	public int monthToday;
 	public int yearToday;
 
-	public void initialize() {
-		
-		DateFormat df = new SimpleDateFormat("MM/dd/yyyy");
-        Date d = new Date();
-        
-		//refreshCalendar();
+	String[] months =  {"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
+	private CalendarDate date = new CalendarDate();
 
+	public void initialize() {
+		GregorianCalendar cal = new GregorianCalendar();
+		DateFormat df = new SimpleDateFormat("MM/dd/yyyy");
+		refreshCalendar(date.getMonthBound(), date.getYearBound());
 	}
 
 
 	public void nextMonth(ActionEvent e) {
-		if (monthToday == 11)
-                    {
-			monthToday = 1;
-			yearToday += 1;
-		}
-		else
-                    {
-			monthToday += 1;
-		}
-		refreshCalendar(monthToday, yearToday);
+		if (date.getMonthToday() == 11) {
+			date.setMonthToday(0);
+			date.setYearToday(date.getYearToday() + 1);
+		} else
+			date.setMonthToday(date.getMonthToday() + 1);
+
+		refreshCalendar(date.getMonthToday(), date.getYearToday());
 }
 	public void lastMonth(ActionEvent e){
-	if (monthToday == 1)
-	        {
-		monthToday = 11;
-		yearToday -= 1;
-	        }
-	else
-	        {
-		monthToday -= 1;
-	        }
-	refreshCalendar(monthToday, yearToday);
+		if (date.getMonthToday() == 0) {
+			date.setMonthToday(11);
+			date.setYearToday(date.getYearToday() - 1);
+		} else
+			date.setMonthToday(date.getMonthToday() - 1);
+
+		refreshCalendar(date.getMonthToday(), date.getYearToday());
 }
 
 
 	public void refreshCalendar(int month, int year){
-		
-		String[] months =  {"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
 		int nod, som, i;
-
+		/*
 		if (month == 0 && year <= yearBound-10)
             btnPrev.setDisable(true);
 		if (month == 11 && year >= yearBound+100)
             btnNext.setDisable(true);
+		*/
 
-
-		dateLabel.setText(months[month - 1] +" "+ Integer.toString(year));
-		
+		dateLabel.setText(months[month] +" "+ Integer.toString(year));
 
 		calendarGrid.getChildren().clear();
 
-
-		GregorianCalendar cal = new GregorianCalendar(year, month - 1, 1);
+		GregorianCalendar cal = new GregorianCalendar(year, month, 1);
 		nod = cal.getActualMaximum(GregorianCalendar.DAY_OF_MONTH);
 		som = cal.get(GregorianCalendar.DAY_OF_WEEK);
 
@@ -89,18 +79,13 @@ public class SecretaryScreenController {
 			int row = new Integer((i+som-2)/7);
 			int column  =  (i+som-2)%7;
 			final int var = i;
-	
+
 
 			Button button = new Button(Integer.toString(i));
 			button.setMinSize(32, 25);
-<<<<<<< HEAD
-            button.setStyle("-fx-font-family: 'Avenir 85 Heavy'; -fx-font-size: 10px; -fx-background-color: transparent; -fx-text-fill: #FFFFFF");
+			button.setStyle("-fx-font-family: 'Avenir 85 Heavy'; -fx-font-size: 10px; -fx-background-color: transparent; -fx-text-fill: #FFFFFF");
 
-			
-			
-=======
 			button.setStyle("-fx-background-color: transparent");
->>>>>>> 4e85d02df335e67ee56b88b0b0710629a55596da
 			button.setOnMouseEntered(new EventHandler<MouseEvent>() {
 				public void handle(MouseEvent event) {
 		            button.setStyle("-fx-font-family: 'Avenir 85 Heavy'; -fx-font-size: 10px; -fx-background-color: transparent; -fx-text-fill: #FFFFFF; -fx-border-color: blue");
